038f74e4d7435efd43eeb3c983da2e0e43fe4d3d
TechieTeee <techieteee@protonmail.com>
2022-10-01 20:22:10 -0500

    Continuing Customization

--- src/App.js
+++ src/App.js
@@ -174,7 +174,7 @@ const App = () => {
             <p className="header">WWW: Wonder Women of the World</p>
             <p className="sub-header">The Most Inspiring Women on the Block</p>
             <div className="moon" />
-            <div className="kiki" />
+            <div className="Shuri" />
         </div>
     );
 
